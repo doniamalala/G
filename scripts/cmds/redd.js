1999bf8152d5e187285a7304485114a565908b54
doniamalala <156112501+doniamalala@users.noreply.github.com>
2024-05-20 08:12:01 +0300

    Create redd.js

---
+++ scripts/cmds/redd.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+
+module.exports = {
+  config: {
+    name: 'redd',
+    version: '1.0',
+    author: 'Bruno',
+    role: 0,
+    category: 'Ai-Chat',
+    shortDescription: {
+      en: `Your ai personal girlfriend`
+    },
+    longDescription: {
+      en: `Your ai personal girlfriend`
+    },
+    guide: {
+      en: '{pn}redd [query]'
+    },
+  },
+
+  onStart: async function ({ api, event, args, usersData }) {
+    try {
+      const query = args.join(" ") || "hello";
+      const { name } = (await usersData.get(event.senderID));
+
+      if (query) {
+        api.setMessageReaction("⏳", event.messageID, (err) => console.log(err), true);
+        const processingMessage = await api.sendMessage(
+          `Asking redd. Please wait a moment...`,
+          event.threadID
+        );
+
+        const apiUrl = `https://liaspark.chatbotcommunity.ltd/@unregistered/api/redd/?query=${encodeURIComponent(query)}`;
+        const response = await axios.get(apiUrl);
+
+        if (response.data && response.data.message) {
+          const trimmedMessage = response.data.message.trim();
+          api.setMessageReaction("✅", event.messageID, (err) => console.log(err), true);
+          await api.sendMessage({ body: trimmedMessage }, event.threadID, event.messageID);
+
+          console.log(`Sent redd's response to the user`);
+        } else {
+          throw new Error(`Invalid or missing response from redd API`);
+        }
+
+        await api.unsendMessage(processingMessage.messageID);
+      }
+    } catch (error) {
+      console.error(`❌ | Failed to get redd's response: ${error.message}`);
+      const errorMessage = `❌ | An error occurred. You can try typing your query again or resending it. There might be an issue with the server that's causing the problem, and it might resolve on retrying.`;
+      api.sendMessage(errorMessage, event.threadID);
+    }
+  },
+};
+
