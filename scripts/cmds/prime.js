4afdfac52118a876039848a4f9115fb3b7ed71a8
doniamalala <156112501+doniamalala@users.noreply.github.com>
2024-05-21 21:12:40 +0300

    Create prime.js

---
+++ scripts/cmds/prime.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+
+module.exports = {
+  config: {
+    name: 'prime',
+    version: '1.0.0',
+    role: 0,
+    hasPrefix: false,
+    description: "Get response from Mistral AI",
+    usage: "prime [query]",
+    credits: 'churchill',
+    cooldown: 3,
+  },
+
+  onStart: async function({ api, event, args }) {
+    const mistralApiUrl = "https://hashier-api-groq.vercel.app/api/groq/mistral";
+
+    try {
+      const query = args.join(" ");
+      if (!query) {
+        api.sendMessage("Please provide a query.", event.threadID, event.messageID);
+        return;
+      }
+
+      const response = await axios.get(`${mistralApiUrl}?ask=${encodeURIComponent(query)}`);
+      const data = response.data.response;
+
+      const formattedResponse = `
+🔮 **💖Bruno❤️ AI Response** 🔮\n
+Query: ${query}
+Response: ${data}
+      `;
+
+      api.sendMessage(formattedResponse, event.threadID, event.messageID);
+    } catch (error) {
+      console.error(error);
+      api.sendMessage("An error occurred while processing the command. Please try again.", event.threadID);
+    }
+  }
+};
